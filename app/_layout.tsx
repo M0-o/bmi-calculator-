3def4995a3091d7c1bd63f4744aa1e742040b403
M0-o <berrami.mouad2@gmail.com>
2025-11-17 18:05:25 +0100

    finished styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import * as SystemUI from "expo-system-ui";
+import { useEffect } from "react";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  useEffect(() => {
+    SystemUI.setBackgroundColorAsync("#a020f0"); // purple
+  }, []);
+
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="#a020f0" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,74 +3,108 @@ import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 
 const IMAGES = {
-  underweight: require('../assets/images/underweight.jpeg'),
-  normal: require('../assets/images/normal.jpeg'),
-  overweight: require('../assets/images/overweight.jpeg'),
-  obese: require('../assets/images/obese.jpeg'),
-  extremely_obese: require('../assets/images/extremely_obese.jpeg'),
+    underweight: require('../assets/images/underweight.jpeg'),
+    normal: require('../assets/images/normal.jpeg'),
+    overweight: require('../assets/images/overweight.jpeg'),
+    obese: require('../assets/images/obese.jpeg'),
+    extremely_obese: require('../assets/images/extremely_obese.jpeg'),
 };
 
-function getPictureKey(imc: number) : keyof typeof IMAGES{
+function getPictureKey(imc: number): keyof typeof IMAGES {
 
-  if(imc < 18.5) return "underweight";
-  if(imc < 24.9) return "normal";
-  if(imc < 29.9) return "overweight";
-  if(imc < 34.9) return "obese";
-  return "extremely_obese";
-  
+    if (imc < 18.5) return "underweight";
+    if (imc < 24.9) return "normal";
+    if (imc < 29.9) return "overweight";
+    if (imc < 34.9) return "obese";
+    return "extremely_obese";
+
 }
 
 export default function Index() {
-  const [weight, setWeight] = useState(0);
-  const [height, setHeight] = useState(0);
-  const [imc, setImc] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [imc, setImc] = useState(0);
 
 
-  function handleButtonPress(){
-    if (height <= 0 || weight <=0){
-    alert("Veuillez entrer une taille valide.");
-    return ;}
-      setImc(weight *10000/ (height * height));
-  }
-  
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>IMC FOR II MASTER BDCC 1</Text>
-      <Image source ={require("../assets/images/imc.jpeg")} style={{width:200 , height:100}} resizeMode="contain"/>
-    <View>
-        <View>
-      <Text>Poids</Text>
-    <TextInput placeholder="Entrez votre poids" keyboardType="numeric" onChange={e => setWeight(Number(e.nativeEvent.text))}/>
-        <Text>Kg</Text>
+    function handleButtonPress() {
+        if (height <= 0 || weight <= 0) {
+            alert("Veuillez entrer une taille valide.");
+            return;
+        }
+        setImc(weight * 10000 / (height * height));
+    }
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#8a4bff15",
+            }}
+        >
+            <Text style={styles.h1}>IMC FOR II MASTER BDCC 1</Text>
+            <Image source={require("../assets/images/imc.jpeg")} style={{ width: 200, height: 100 }} resizeMode="contain" />
+            <View>
+                <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
+                    <Text style={styles.inputLabel}>Poids</Text>
+                    <TextInput placeholder="Entrez votre poids" value={weight == 0 ? null : weight.toString()} keyboardType="numeric" onChange={e => setWeight(Number(e.nativeEvent.text))} style={styles.input} />
+                    <Text style={styles.inputLabel}>Kg</Text>
+                </View>
+                <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
+                    <Text style={styles.inputLabel}>Taille</Text>
+                    <TextInput placeholder="Entrez votre taille" value={height == 0 ? null : height.toString()} keyboardType="numeric" onChange={e => setHeight(Number(e.nativeEvent.text))} style={styles.input} />
+                    <Text style={styles.inputLabel}>Cm</Text>
+                </View>
+                <TouchableOpacity
+                    onPress={handleButtonPress}
+                    style={{
+                        backgroundColor: "#a020f0",
+                        paddingVertical: 12,
+                        paddingHorizontal: 30,
+                        borderRadius: 50,
+                        marginTop: 10,
+                    }}
+                >
+                    <Text style={{ color: "white", fontSize: 16, textAlign: "center" }}>Calculer IMC</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={styles.h3}> Votre IMC est : {imc == 0 ? "" : imc.toPrecision(4)}</Text>
+            {imc && <Image source={IMAGES[getPictureKey(imc)]} style={{ width: 200, height: 400 }} resizeMode="contain" />}
         </View>
-        <View>
-            <Text>Taille</Text>
-    <TextInput placeholder="Entrez votre taille" keyboardType="numeric" onChange={e => setHeight(Number(e.nativeEvent.text))}/>
-<Text>Cm</Text>
-        </View>
-<TouchableOpacity
-  onPress={handleButtonPress}
-  style={{
-    backgroundColor: "#a020f0", 
-    paddingVertical: 12,
-    paddingHorizontal: 30,
-    borderRadius: 50,
-    marginTop: 10,
-  }}
->
-  <Text style={{ color: "white", fontSize: 16 }}>Calculer IMC</Text>
-</TouchableOpacity>
-    </View>
-      <Text> Votre IMC est : {imc == 0 ? "": imc.toPrecision(4)}</Text>
-      {imc && <Image source={IMAGES[getPictureKey(imc)]} style={{width:200, height:400}} resizeMode="contain"/>}
-    </View>
-  );
+    );
 }
 
+
+const styles = {
+    h1: {
+        fontSize: 24,
+        fontWeight: "600",
+        marginBottom: 20,
+    },
+    h2: {
+        fontSize: 20,
+        fontWeight: "600",
+        marginBottom: 15,
+    },
+    h3: {
+        fontSize: 16,
+        fontWeight: "600",
+        marginBottom: 10,
+    },
+    inputLabel: {
+        fontSize: 14,
+        marginBottom: 5,
+    },
+    input: {
+        color: "black",
+        padding: 10,
+        borderBottomWidth:1 ,
+        borderBottomColor: "black",
+        marginBottom: 10,
+        textAlign: "center",
+        width: 150,
+    }
+
+}
 

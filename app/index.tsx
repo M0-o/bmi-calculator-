0afed3071cdc5bd997e0c1c41671ef3e7f544a7a
M0-o <berrami.mouad2@gmail.com>
2025-11-17 15:51:32 +0100

    base functionality implemented

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [imc, setImc] = useState(0);
   return (
     <View
       style={{
@@ -9,7 +13,17 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <h1>IMC FOR II MASTER BDCC 1</h1>
+    <View>
+    <TextInput placeholder="Entrez votre poids" keyboardType="numeric" onChange={e => setWeight(Number(e.nativeEvent.text))}/>
+    <TextInput placeholder="Entrez votre taille" keyboardType="numeric" onChange={e => setHeight(Number(e.nativeEvent.text))}/>
+
+<Button title="Calculer IMC" onPress={() => {
+  setImc(weight *10000/ (height * height));
+}}></Button>
+    </View>
+      <h2> Votre IMC est : {imc == 0 ? "" : imc.toPrecision(4)}</h2>
+      
     </View>
   );
 }
